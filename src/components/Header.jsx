48e8d74562bcdc454b189e949306b8afd25cf735
dipeshb1 <dipeshb164@gmail.com>
2020-05-03 19:11:33 +0530

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+import NoteIcon from "@material-ui/icons/Note";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        AddaNote
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
